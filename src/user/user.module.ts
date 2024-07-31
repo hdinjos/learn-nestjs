ba123bd59e8f0c3708b521b1733a288034ae6e56
Hendi Saputra <49345921+hdinjos@users.noreply.github.com>
2024-07-31 14:57:13 +0700

    fix code by linter

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,9 +1,10 @@
-import { Module } from '@nestjs/common';
-import { UserService } from './user.service';
-import { UserController } from './user.controller';
+import { Module } from "@nestjs/common";
+import { UserService } from "./user.service";
+import { UserController } from "./user.controller";
+import { Connection } from "./connection/connection";
 
 @Module({
-  providers: [UserService],
-  controllers: [UserController]
+  providers: [UserService, Connection],
+  controllers: [UserController],
 })
 export class UserModule {}
